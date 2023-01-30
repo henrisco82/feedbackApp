cd33c2bbdb052ed3491508c3491deffbc5928055
Henry Unah <Henry-Odohi.Unah@external.telekom.de>
2023-01-30 21:49:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import About from './pages/About'
+import { FeedbackProvider } from './context/FeedbackContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <FeedbackProvider>
+            <Router>
+                <Header />
+                <div className='container'>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm  />
+                            <FeedbackStats />
+                            <FeedbackList  />
+                            <AboutIconLink />
+                        </>
+                    }>             
+                    </Route>
+                    <Route path='/about' element={<About />}></Route>
+                </Routes>  
+                </div>
+            </Router>
+        </FeedbackProvider>
+        
+    )
 }
 
 export default App;
